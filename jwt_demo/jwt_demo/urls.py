427321822e9d3f249fb92b73a821cec3481a91cd
Mario Llobet <mllobetpalma@santandertecnologia.com.ar>
2021-05-01 20:36:15 -0300

    remove some code, more vanilla to make it easier.

--- jwt_demo/jwt_demo/urls.py
+++ jwt_demo/jwt_demo/urls.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import (
+    path,
+    include,
+)
 
 from rest_framework_simplejwt import views as jwt_views
 from demo_jwt_app import views
@@ -7,11 +10,10 @@ from demo_jwt_app import views
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    path('api-auth/', include('rest_framework.urls')),
-
     # JWT
     path('api/token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/verify/', jwt_views.TokenVerifyView.as_view(), name='token_verify'),
 
     # Demo test view
     path('demo/', views.DemoView.as_view(), name='demo'),
