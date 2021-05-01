5194c3f146464788fa5512427123cbe5429619e1
Mario Llobet <mllobetpalma@santandertecnologia.com.ar>
2021-05-01 17:38:24 -0300

    view rename

--- jwt_demo/jwt_demo/urls.py
+++ jwt_demo/jwt_demo/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     # JWT
     path('api/token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
-    # Test demo view
-    path('demo/', views.DemoView.as_view(), name='test'),
+
+    # Demo test view
+    path('demo/', views.DemoView.as_view(), name='demo'),
 ]
